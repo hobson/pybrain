0ac1621cf981337e3d3544825cedd7e291be0877
Hobson Lane <hobsonlane@gmail.com>
2015-05-14 14:06:11 -0700

    maintainers added to setup to satisfy pypi

--- pybrain/datasets/__init__.py
+++
@@ -1,8 +0,0 @@
-# $Id$
-from pybrain.datasets.sequential import SequentialDataSet
-from pybrain.datasets.supervised import SupervisedDataSet
-from pybrain.datasets.supervised import SequentialSupervisedDataSet
-from pybrain.datasets.unsupervised import UnsupervisedDataSet
-from pybrain.datasets.importance import ImportanceDataSet
-from pybrain.datasets.reinforcement import ReinforcementDataSet
-from pybrain.datasets.classification import ClassificationDataSet, SequenceClassificationDataSet
