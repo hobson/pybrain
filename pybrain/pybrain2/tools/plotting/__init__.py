299ea8b102108ef9774c97c59a6667bf7f3c2816
Hobson Lane <hobsonlane@gmail.com>
2015-05-18 08:58:14 -0700

    .gitignore

---
+++ pybrain/pybrain2/tools/plotting/__init__.py
@@ -0,0 +1,4 @@
+#from fitnesslandscapes import FitnessPlotter
+from .multiline import MultilinePlotter
+from .colormaps import ColorMap, ColorMaps
+from .fitnessprogression import plotFitnessProgession
